8d3d05e7ed79f29e5aa02c7e0ceecd32009e63fc
Anrolette <anrolette.hollamby@gmail.com>
2021-04-29 07:33:53 +0200

    Updated Gtag

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -30,27 +30,30 @@ Have a look at ./pages/index.js to see where we pass props through to this Layou
 const Layout = (props) => (
   <div>
     <Head>
-<script 
-dangerouslySetInnerHTML={{ __html: 
-`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M97GSM5');`
-}}></script>
       <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossOrigin="anonymous"
       />
+
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-XEXHM0BFXL"
+  />
+
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XEXHM0BFXL');
+        `,
+    }}
+  />
     </Head>
     <div style={layoutStyle}>
-    <noscript 
-dangerouslySetInnerHTML={{ __html: 
-`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M97GSM5"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-}}></noscript>
       <Header />
       {props.children}
     </div>
